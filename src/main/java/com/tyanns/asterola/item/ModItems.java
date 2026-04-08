c46406d0b6deca57a4db2eac7a6352102231ff0c
TyannS <cth20030624@gmail.com>
2026-04-09 03:13:22 +0800

    Initial commit

--- src/main/java/com/tyanns/asterola/item/ModItems.java
+++ src/main/java/com/tyanns/asterola/item/ModItems.java
@@ -1,13 +1,11 @@
 package com.tyanns.asterola.item;
 
 import com.tyanns.asterola.Asterola;
-import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 
 import java.util.function.Function;
