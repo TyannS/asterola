85635fa8c1c45ccaa9cad6c08c4968b68466102d
TyannS <cth20030624@gmail.com>
2026-04-24 16:38:48 +0800

    Initial commit

--- src/main/java/com/tyanns/asterola/datagen/ModModelsProvider.java
+++ src/main/java/com/tyanns/asterola/datagen/ModModelsProvider.java
@@ -2,28 +2,13 @@ package com.tyanns.asterola.datagen;
 
 import com.tyanns.asterola.block.ModBlocks;
 import com.tyanns.asterola.item.ModItems;
-import com.tyanns.asterola.trim.ModTrimMaterials;
 import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
 import net.minecraft.client.data.models.model.*;
-import net.minecraft.client.renderer.item.SelectItemModel;
-import net.minecraft.client.renderer.item.properties.select.TrimMaterialProperty;
-import net.minecraft.client.resources.model.cuboid.ItemModelGenerator;
-import net.minecraft.client.resources.model.sprite.Material;
-import net.minecraft.resources.Identifier;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.util.Util;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.equipment.EquipmentAsset;
 import net.minecraft.world.item.equipment.EquipmentAssets;
-import net.minecraft.world.item.equipment.trim.MaterialAssetGroup;
-import net.minecraft.world.item.equipment.trim.TrimMaterial;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ModModelsProvider extends FabricModelProvider {
     public ModModelsProvider(FabricPackOutput output) {
